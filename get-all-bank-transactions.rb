e2af82dbe3fbd19074e3f3e5555135462e7685cd
Jason Fox <jasonrobertfox@gmail.com>
2015-07-09 10:35:36 -0400

    changed to pipe delimit and multiple accounts.

--- get-all-bank-transactions.rb
+++ get-all-bank-transactions.rb
@@ -8,107 +8,110 @@ require 'json'
 require 'open-uri'
 require 'fileutils'
 
-authtoken=YOUR_AUTH_TOKEN
-organization_id=YOUR_ORGANIZATION_ID
+authtoken=''
+organization_id=
 base_endpoint = 'https://books.zoho.com/api/v3'
 
-account_id=YOUR_ACCOUNT_NUMBER
+account_ids=[]
 transactions_endpoint = base_endpoint + '/banktransactions'
 
 
-output_filename = './some-name.csv'
+output_filename = './consolidated-export-2.txt'
 output = open(output_filename, 'w')
 
-page = 1
-has_more = true
+account_ids.each do |account_id|
 
-while has_more
-  transactions = RestClient.get transactions_endpoint, { params: { authtoken: authtoken, organization_id: organization_id, account_id: account_id, page: page}}
-  transactions = JSON.parse(transactions)
-  has_more = transactions['page_context']['has_more_page']
-  transactions['banktransactions'].each do |transaction|
+  page = 1
+  has_more = true
+
+  while has_more
+    transactions = RestClient.get transactions_endpoint, { params: { authtoken: authtoken, organization_id: organization_id, account_id: account_id, page: page}}
+    transactions = JSON.parse(transactions)
+    has_more = transactions['page_context']['has_more_page']
     count = 0
-    if count == 140
-      puts "gonna take a break for a min...\n"
-      count = 0
-      sleep(60)
-      puts "ok doke!\n"
-    end
-    count+=1
-    line = []
-    puts transaction['transaction_id']
-    #get the basic transaction data
-    line << transaction['transaction_id']
-    line << transaction['date']
-    line << transaction['amount']
-    line << transaction['transaction_type']
-    line << transaction['status']
-    line << transaction['source']
-    line << transaction['account_name']
-    line << transaction['account_type']
-    line << transaction['payee']
-    line << transaction['description']
-    line << transaction['debit_or_credit']
-    line << transaction['offset_account_name']
-    line << transaction['reference_number']
-    line << transaction['debit_or_credit']
+    transactions['banktransactions'].each do |transaction|
+      if count == 140
+        puts 'gonna rest for a bit...'
+        sleep(60)
+        puts 'ok back to work!'
+        count = 0
+      end
+      unless transaction['transaction_type'] == 'customer_payment'
+        line = []
+        puts transaction['transaction_id']
+        #get the basic transaction data
+        line << transaction['transaction_id']
+        line << transaction['date']
+        line << transaction['amount']
+        line << transaction['transaction_type']
+        line << transaction['status']
+        line << transaction['source']
+        line << transaction['account_name']
+        line << transaction['account_type']
+        line << transaction['payee']
+        line << transaction['description']
+        line << transaction['debit_or_credit']
+        line << transaction['offset_account_name']
+        line << transaction['reference_number']
 
-    # if there is a customer id, get that
-    if transaction['customer_id'] == ''
-      line << ''
-      line << ''
-    else
-      count+=1
-      customer = JSON.parse(RestClient.get("#{base_endpoint}/contacts/#{transaction['customer_id']}", { params: { authtoken: authtoken, organization_id: organization_id }}))
-      line << customer['contact']['contact_name']
-      line << customer['contact']['company_name']
-    end
+        # if there is a customer id, get that
+        if transaction['customer_id'] == ''
+          line << ''
+          line << ''
+        else
+          count +=1
+          customer = JSON.parse(RestClient.get("#{base_endpoint}/contacts/#{transaction['customer_id']}", { params: { authtoken: authtoken, organization_id: organization_id }}))
+          line << customer['contact']['contact_name']
+          line << customer['contact']['company_name']
+        end
 
-    # if the transaction is an expense get that data, otherwise get the other kind
-    if transaction['transaction_type'] == 'expense'
-      count+=1
-      expense = JSON.parse(RestClient.get("#{base_endpoint}/expenses/#{transaction['transaction_id']}", { params: { authtoken: authtoken, organization_id: organization_id}}))
-      line << expense['expense']['vendor_name']
-      line << expense['expense']['reference_number']
-      line << expense['expense']['description']
-      line << expense['expense']['is_billable']
-      line << expense['expense']['customer_name']
-      line << expense['expense']['project_name']
-
-    elsif transaction['transaction_type'] == 'vendor_payment'
-      payment = JSON.parse(RestClient.get("#{base_endpoint}/vendorpayments/#{transaction['transaction_id']}", { params: { authtoken: authtoken, organization_id: organization_id}}))
-      line << payment['vendorpayment']['vendor_name']
-      line << payment['vendorpayment']['reference_number']
-      line << payment['vendorpayment']['description']
-      line << ''
-      line << ''
-      line << ''
-      line << ''
-      line << ''
-    else
-      count+=1
-      begin
-        nonexpense = JSON.parse(RestClient.get("#{transactions_endpoint}/#{transaction['transaction_id']}", { params: { authtoken: authtoken, organization_id: organization_id, account_id: account_id}}))
-        line << nonexpense['banktransaction']['vendor_name']
-        line << nonexpense['banktransaction']['reference_number']
-        line << nonexpense['banktransaction']['description']
-        line << ''
-        line << nonexpense['banktransaction']['customer_name']
-        line << ''
-        line << nonexpense['banktransaction']['from_account_name']
-        line << nonexpense['banktransaction']['to_account_name']
-      rescue
-        puts "Failed --> #{transaction['transaction_id']}"
-        puts transaction.inspect
+        # if the transaction is an expense get that data, otherwise get the other kind
+        if transaction['transaction_type'] == 'expense'
+          count +=1
+          expense = JSON.parse(RestClient.get("#{base_endpoint}/expenses/#{transaction['transaction_id']}", { params: { authtoken: authtoken, organization_id: organization_id}}))
+          line << expense['expense']['vendor_name']
+          line << expense['expense']['reference_number']
+          line << expense['expense']['description']
+          line << expense['expense']['is_billable']
+          line << expense['expense']['customer_name']
+          line << expense['expense']['project_name']
+          line << ''
+          line << ''
+        elsif transaction['transaction_type'] == 'vendor_payment'
+          count +=1
+          payment = JSON.parse(RestClient.get("#{base_endpoint}/vendorpayments/#{transaction['transaction_id']}", { params: { authtoken: authtoken, organization_id: organization_id}}))
+          line << payment['vendorpayment']['vendor_name']
+          line << payment['vendorpayment']['reference_number']
+          line << payment['vendorpayment']['description']
+          line << ''
+          line << ''
+          line << ''
+          line << ''
+          line << ''
+        else
+          begin
+            count +=1
+            nonexpense = JSON.parse(RestClient.get("#{transactions_endpoint}/#{transaction['transaction_id']}", { params: { authtoken: authtoken, organization_id: organization_id, account_id: account_id}}))
+            line << nonexpense['banktransaction']['vendor_name']
+            line << nonexpense['banktransaction']['reference_number']
+            line << nonexpense['banktransaction']['description']
+            line << ''
+            line << nonexpense['banktransaction']['customer_name']
+            line << ''
+            line << nonexpense['banktransaction']['from_account_name']
+            line << nonexpense['banktransaction']['to_account_name']
+          rescue
+            puts "Failed --> #{transaction['transaction_id']}"
+            puts transaction.inspect
+          end
+        end
+        output.puts line.join('|')
       end
     end
 
-
-      output.puts line.join(',')
+    puts "processed #{page} page\n"
+    page += 1
   end
-
-  puts "processed #{page} page\n"
-  page += 1
 end
 
 output.close
